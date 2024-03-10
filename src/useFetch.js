9bbb5e2a86782285a8fcdad82fca966ebd17a46b
krishna panthi <krishnapanthi94@gmail.com>
2024-03-11 03:09:34 +0545

    files from day 2

---
+++ src/useFetch.js
@@ -0,0 +1,34 @@
+
+import {useState, useEffect} from 'react';
+
+const useFetch=(url)=>{
+    const [data, setData] = useState(null);
+    const[isPending, setIsPending]=useState(true);
+    const [error, setError]= useState(null)
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            fetch(url)
+            .then(res=>{
+    
+               if(!res.ok){
+                throw Error('Could not fetch the data for the resource');
+               }
+               return res.json()
+            })
+            .then((data)=>{
+                setData(data)
+                setIsPending(false)
+                setError(null);
+            })
+            .catch(err => {
+                setIsPending(false)
+           setError(err.message);
+            })
+        },1000);     //to simulate the idea of taking little bit longer to fetch data
+       },[url]);  
+
+ return {data, isPending, error}
+}
+
+export default useFetch;
